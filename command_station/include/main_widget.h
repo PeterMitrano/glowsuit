4176b5ee30b30a8559bb7fe94e968bc7cfe6c234
Peter Mitrano <mitranopeter@gmail.com>
2020-03-10 15:13:36 -0400

    working on UI for new static choreo

--- command_station/include/main_widget.h
+++ command_station/include/main_widget.h
@@ -4,6 +4,7 @@
 #include <map>
 #include <optional>
 
+#include <QCheckBox>
 #include <QCloseEvent>
 #include <QMediaPlayer>
 #include <QObject>
@@ -16,7 +17,6 @@
 #include <../src/ui_mainwidget.h>
 
 #include <midi/live_midi_input.h>
-#include <midi/midi_file_player.h>
 #include <serial/serial.h>
 #include <visualizer.h>
 
@@ -24,14 +24,12 @@ std::optional<json> load_suit_description();
 
 QString select_music_file(QWidget *parent);
 
-QString select_midi_file(QWidget *parent);
-
 class MainWidget : public QWidget
 {
 Q_OBJECT
 
 public:
-    virtual ~MainWidget();
+    ~MainWidget() override;
 
     explicit MainWidget(QWidget *parent = nullptr);
 
@@ -43,11 +41,7 @@ public:
 
     void keyReleaseEvent(QKeyEvent *event) override;
 
-    void set_state(QMediaPlayer::State state);
-
-    QMediaPlayer::State state() const;
-
-    void update_duration_info(qint64 currentInfo);
+    [[nodiscard]] QMediaPlayer::State state() const;
 
     void handle_cursor(QMediaPlayer::MediaStatus status);
 
@@ -55,27 +49,15 @@ public:
 
 public slots:
 
-    void event_count_changed(int event_count);
-
-    void num_tracks_changed(int num_tracks);
+    void num_suits_changed(int value);
 
     void update_serial_port_list();
 
     void live_midi_changed(int state);
 
-    void play_pause_clicked();
-
-    void midi_file_button_clicked();
-
     void music_file_button_clicked();
 
     void xbee_port_changed(int index);
-
-    void seek(int milliseconds);
-
-    void duration_changed(qint64 duration);
-
-    void position_changed(qint64 progress);
 
     void status_changed(QMediaPlayer::MediaStatus status);
 
@@ -87,9 +69,11 @@ public slots:
 
     void all_off_clicked();
 
-signals:
+    void start_clicked();
 
-    void midi_file_changed(QString midi_filename);
+    void abort_clicked();
+
+signals:
 
     void play();
 
@@ -111,14 +95,11 @@ public:
     serial::Serial *xbee_serial{nullptr};
     QSettings *settings;
 
-
 private:
     Ui_MainWidget ui;
 
     QMediaPlayer::State player_state = QMediaPlayer::StoppedState;
     QMediaPlayer *music_player{nullptr};
-    QThread midi_player_thread;
-    MidiFilePlayer midi_file_player;
 
     qint64 song_duration_ms{0};
     QTimer *timer{nullptr};
@@ -126,4 +107,6 @@ private:
     bool controls_hidden{false};
     QLayoutItem *controls_layout_item{nullptr};
     QString previously_selected_port_name;
+    std::vector<QCheckBox *> suit_status_checkboxes;
+
 };
--- command_station/src/main_widget.cpp
+++ command_station/src/main_widget.cpp
@@ -1,4 +1,3 @@
-#include <QCheckBox>
 #include <QComboBox>
 #include <QFileDialog>
 #include <QMessageBox>
@@ -9,17 +8,11 @@
 
 #include <common.h>
 #include <main_widget.h>
-#include <midi/midi_file_player.h>
 
 MainWidget::MainWidget(QWidget *parent)
         : QWidget(parent)
 {
     ui.setupUi(this);
-
-    ui.play_button->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-    ui.stop_button->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
-    connect(ui.play_button, &QAbstractButton::clicked, this, &MainWidget::play_pause_clicked);
-    connect(ui.stop_button, &QAbstractButton::clicked, this, &MainWidget::stop);
 
     // these are pointers because you can't use "this" in an the constructor initializer list
     visualizer = new Visualizer(this);
@@ -29,47 +22,23 @@ MainWidget::MainWidget(QWidget *parent)
     music_player = new QMediaPlayer(this);
     music_player->setNotifyInterval(100);
 
-    midi_file_player.start_thread();
-    connect(this, &MainWidget::midi_file_changed, &midi_file_player, &MidiFilePlayer::midi_file_changed);
-    connect(&midi_file_player, &MidiFilePlayer::midi_event, visualizer, &Visualizer::on_midi_file_event);
-    ui.player_slider->setRange(0, static_cast<int>(music_player->duration()));
-
-    connect(ui.player_slider, &QSlider::sliderMoved, this, &MainWidget::seek);
-    connect(ui.player_slider, &QSlider::sliderMoved, &midi_file_player, &MidiFilePlayer::seek);
-    connect(music_player, &QMediaPlayer::durationChanged, this, &MainWidget::duration_changed);
-    connect(music_player, &QMediaPlayer::positionChanged, this, &MainWidget::position_changed);
     connect(music_player, &QMediaPlayer::mediaStatusChanged, this, &MainWidget::status_changed);
-    connect(music_player, &QMediaPlayer::mediaStatusChanged, &midi_file_player, &MidiFilePlayer::status_changed);
     connect(music_player, QOverload<QMediaPlayer::Error>::of(&QMediaPlayer::error), this,
             &MainWidget::display_error_message);
 
-    connect(this, &MainWidget::play, music_player, &QMediaPlayer::play);
-    connect(this, &MainWidget::pause, music_player, &QMediaPlayer::pause);
-    connect(this, &MainWidget::stop, music_player, &QMediaPlayer::stop);
-    connect(this, &MainWidget::play, &midi_file_player, &MidiFilePlayer::play);
-    connect(this, &MainWidget::pause, &midi_file_player, &MidiFilePlayer::pause);
-    connect(this, &MainWidget::stop, &midi_file_player, &MidiFilePlayer::stop);
-    connect(music_player, &QMediaPlayer::stateChanged, this, &MainWidget::set_state);
-
     connect(ui.front_button, &QPushButton::clicked, visualizer, &Visualizer::front_status_clicked);
     connect(ui.back_button, &QPushButton::clicked, visualizer, &Visualizer::back_status_clicked);
-    connect(&midi_file_player, &MidiFilePlayer::num_tracks_changed, this, &MainWidget::num_tracks_changed);
-    connect(ui.track_spinbox, qOverload<int>(&QSpinBox::valueChanged), &midi_file_player,
-            &MidiFilePlayer::track_changed);
     connect(ui.scale_spinbox, qOverload<double>(&QDoubleSpinBox::valueChanged), visualizer,
             &Visualizer::viz_scale_changed);
     connect(ui.select_music_file_button, &QPushButton::clicked, this, &MainWidget::music_file_button_clicked);
-    connect(ui.select_midi_file_button, &QPushButton::clicked, this, &MainWidget::midi_file_button_clicked);
     connect(ui.all_on_button, &QPushButton::clicked, this, &MainWidget::all_on_clicked);
     connect(ui.all_off_button, &QPushButton::clicked, this, &MainWidget::all_off_clicked);
     connect(ui.live_checkbox, &QCheckBox::stateChanged, this, &MainWidget::live_midi_changed);
-    connect(&midi_file_player, &MidiFilePlayer::event_count_changed, this, &MainWidget::event_count_changed);
-    connect(ui.octave_spinbox, qOverload<int>(&QSpinBox::valueChanged), &midi_file_player,
-            &MidiFilePlayer::octave_spinbox_changed);
     connect(ui.octave_spinbox, qOverload<int>(&QSpinBox::valueChanged), live_midi_worker,
             &LiveMidiWorker::octave_spinbox_changed);
-
-    set_state(music_player->state());
+    connect(ui.num_suits_spinbox, qOverload<int>(&QSpinBox::valueChanged), this, &MainWidget::num_suits_changed);
+    connect(ui.start_button, &QPushButton::clicked, this, &MainWidget::start_clicked);
+    connect(ui.abort_button, &QPushButton::clicked, this, &MainWidget::abort_clicked);
 
     // start a thread for receiving MIDI
     live_midi_worker->moveToThread(&live_midi_thread);
@@ -110,10 +79,6 @@ MainWidget::~MainWidget()
     live_midi_thread.quit();
     live_midi_thread.wait();
 
-    midi_player_thread.requestInterruption();
-    midi_player_thread.quit();
-    midi_player_thread.wait();
-
     // delete pointers that have no parents
     delete live_midi_worker;
 }
@@ -133,21 +98,6 @@ void MainWidget::music_file_button_clicked()
 QString select_music_file(QWidget *parent)
 {
     return QFileDialog::getOpenFileName(parent, "Open Music File", QString(), "*.wav");
-}
-
-void MainWidget::midi_file_button_clicked()
-{
-    midi_filename = select_midi_file(ui.select_midi_file_button);
-    if (!midi_filename.isNull())
-    {
-        ui.midi_filename_label->setText(midi_filename);
-        emit midi_file_changed(midi_filename);
-    }
-}
-
-QString select_midi_file(QWidget *parent)
-{
-    return QFileDialog::getOpenFileName(parent, "Open MIDI File", QString(), "*.mid");
 }
 
 void MainWidget::live_midi_changed(int state)
@@ -171,7 +121,6 @@ void MainWidget::xbee_port_changed(int index)
 
     // TODO: is this an error? possible data race
     live_midi_worker->xbee_serial = xbee_serial;
-    midi_file_player.xbee_serial = xbee_serial;
 }
 
 void MainWidget::closeEvent(QCloseEvent *event)
@@ -185,7 +134,7 @@ void MainWidget::save_settings()
     settings->setValue("gui/octave", ui.octave_spinbox->value());
     settings->setValue("gui/live_checkbox", ui.live_checkbox->isChecked());
     settings->setValue("gui/scale", ui.scale_spinbox->value());
-    settings->setValue("gui/track", ui.track_spinbox->value());
+    settings->setValue("gui/num_suits", ui.num_suits_spinbox->value());
     settings->setValue("gui/controls_hidden", controls_hidden);
     settings->setValue("files/music", music_filename);
     settings->setValue("files/midi", midi_filename);
@@ -200,16 +149,13 @@ void MainWidget::restore_settings()
 
     music_filename = settings->value("files/music").toString();
     ui.music_filename_label->setText(music_filename);
-    midi_filename = settings->value("files/midi").toString();
-    ui.midi_filename_label->setText(midi_filename);
 
     music_player->setMedia(QUrl::fromLocalFile(music_filename));
-    emit midi_file_changed(midi_filename);
 
     ui.octave_spinbox->setValue(settings->value("gui/octave").toInt());
     ui.scale_spinbox->setValue(settings->value("gui/scale").toDouble());
     ui.live_checkbox->setChecked(settings->value("gui/live_checkbox").toBool());
-    ui.track_spinbox->setValue(settings->value("gui/track").toInt());
+    ui.num_suits_spinbox->setValue(settings->value("gui/num_suits").toInt());
     controls_hidden = settings->value("gui/controls_hidden").toBool();
     ui.controls_widget->setVisible(!controls_hidden);
 
@@ -233,85 +179,6 @@ QMediaPlayer::State MainWidget::state() const
     return player_state;
 }
 
-void MainWidget::set_state(QMediaPlayer::State state)
-{
-    if (state != player_state)
-    {
-        player_state = state;
-
-        switch (state)
-        {
-            case QMediaPlayer::StoppedState:
-                ui.stop_button->setEnabled(false);
-                ui.play_button->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-                break;
-            case QMediaPlayer::PlayingState:
-                ui.stop_button->setEnabled(true);
-                ui.play_button->setIcon(style()->standardIcon(QStyle::SP_MediaPause));
-                break;
-            case QMediaPlayer::PausedState:
-                ui.stop_button->setEnabled(true);
-                ui.play_button->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
-                break;
-        }
-    }
-}
-
-void MainWidget::play_pause_clicked()
-{
-    switch (player_state)
-    {
-        case QMediaPlayer::StoppedState:
-        case QMediaPlayer::PausedState:
-            emit play();
-            break;
-        case QMediaPlayer::PlayingState:
-            emit pause();
-            break;
-    }
-}
-
-void MainWidget::seek(int milliseconds)
-{
-    music_player->setPosition(milliseconds);
-}
-
-void MainWidget::duration_changed(qint64 duration)
-{
-    this->song_duration_ms = duration;
-    ui.player_slider->setMaximum(this->song_duration_ms);
-}
-
-void MainWidget::position_changed(qint64 progress)
-{
-    if (!ui.player_slider->isSliderDown())
-        ui.player_slider->setValue(progress);
-
-    update_duration_info(progress);
-}
-
-void MainWidget::update_duration_info(qint64 time_ms)
-{
-    QString time_str;
-    if (time_ms || song_duration_ms)
-    {
-        QTime current_time(static_cast<int>(time_ms / 3600000) % 60,
-                           static_cast<int>(time_ms / 60000) % 60,
-                           static_cast<int>(time_ms / 1000) % 60,
-                           static_cast<int>(time_ms) % 1000);
-        QTime total_time(static_cast<int>(song_duration_ms / 3600000) % 60,
-                         static_cast<int>(song_duration_ms / 60000) % 60,
-                         static_cast<int>(song_duration_ms / 1000) % 60,
-                         static_cast<int>(song_duration_ms) % 1000);
-        QString format = "mm:ss";
-        if (song_duration_ms > 3600000)
-        {
-            format = "hh:mm:ss";
-        }
-        time_str = current_time.toString(format) + " / " + total_time.toString(format);
-    }
-    ui.player_time_label->setText(time_str);
-}
 
 void MainWidget::status_changed(QMediaPlayer::MediaStatus status)
 {
@@ -350,7 +217,9 @@ void MainWidget::update_serial_port_list()
     auto probably_not_serial = [&](serial::PortInfo const &new_port)
     {
         return (new_port.description.find("Serial Port") == std::string::npos) &&
-               (new_port.description.find("USB") == std::string::npos);
+               (new_port.description.find("USB") == std::string::npos) &&
+                (new_port.description.find("usbmodem") == std::string::npos)
+               ;
     };
     new_ports.erase(std::remove_if(new_ports.begin(), new_ports.end(), probably_not_serial), new_ports.end());
 
@@ -389,17 +258,6 @@ void MainWidget::blink_midi_indicator()
     {
         ui.midi_indicator_button->setEnabled(false);
     });
-}
-
-void MainWidget::num_tracks_changed(int const num_tracks)
-{
-    ui.track_spinbox->setRange(0, num_tracks - 1);
-}
-
-void MainWidget::event_count_changed(int const event_count)
-{
-    auto const str = QString("%1 Events").arg(event_count);
-    ui.event_count_label->setText(str);
 }
 
 void MainWidget::all_on_clicked()
@@ -446,4 +304,42 @@ void MainWidget::keyReleaseEvent(QKeyEvent *event)
         ui.controls_widget->setVisible(!controls_hidden);
     }
 
+}
+
+void MainWidget::num_suits_changed(int value)
+{
+    for (auto *checkbox : suit_status_checkboxes)
+    {
+        ui.suit_checkbox_layout->removeWidget(checkbox);
+        checkbox->deleteLater();
+    }
+    suit_status_checkboxes.clear();
+
+    for (auto i=0; i < value; ++i)
+    {
+        auto *checkbox = new QCheckBox(this);
+        checkbox->setText(QString::number(i));
+        checkbox->setCheckable(false);
+        suit_status_checkboxes.emplace_back(checkbox);
+        ui.suit_checkbox_layout->addWidget(checkbox);
+    }
+}
+
+void MainWidget::abort_clicked()
+{
+    // abort!!!!
+}
+
+void MainWidget::start_clicked()
+{
+    // repeatedly send a message containing the current time
+    if (not xbee_serial)
+    {
+        return;
+    }
+
+    State all_on_data;
+    all_on_data.data.fill(0xFF);
+    auto const[packet, size] = make_packet(all_on_data);
+    xbee_serial->write(time.data(), size);
 }
