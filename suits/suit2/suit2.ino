0627dd12153d0c3028319ce1ccb8dfe5fb25fcad
Peter Mitrano <mitranopeter@gmail.com>
2020-03-10 02:11:57 -0400

    static choreo arduino stuff basically implemented

--- suits/suit2/suit2.ino
+++ suits/suit2/suit2.ino
@@ -27,16 +27,14 @@ constexpr  int channel_to_pin[8] = {
   9,  // 7
 };
 
-constexpr int ni_req_buff_len = 8;
-constexpr uint8_t ni_req_buff[ni_req_buff_len] = {0x7E, 0x00, 0x04, 0x08, 0x01, 0x4E, 0x49, 0x5F};
-constexpr int ni_res_buff_len = 10;
 
-uint8_t get_suit_number();
+unsigned int time_offset = 0;
+
 void flashLed(unsigned int on_time);
 
 void setup() {
   delay(1000);
-   
+
   pinMode(led_pin, OUTPUT);
   for (auto const pin : channel_to_pin) {
     pinMode(pin, OUTPUT);
@@ -45,13 +43,8 @@ void setup() {
   Serial.begin(57600);
   nss.begin(9600);
 
-//  for (byte k = 0; k < 5; k++) {
-//    auto const displayInt = pgm_read_byte_near(choreo + k);
-//    Serial.println(displayInt, HEX);
-//  }
-//  Serial.println();
   for (auto i{0u}; i < suit_number; ++i) {
-      flashLed(100);
+    flashLed(100);
   }
 
   xbee.setSerial(Serial);
@@ -63,48 +56,42 @@ void setup() {
   uint16_t hb = *rx16.getData();
   uint16_t lb = *rx16.getData();
   uint16_t current_global_time = hb << 8 + lb;
+  time_offset = current_global_time - millis();
   flashLed(1000);
-  
+
   // respond with an acknowledgement message containing this suit's number (1 byte)
   uint8_t payload[] = { suit_number };
   Tx16Request tx = Tx16Request(0x00, payload, sizeof(payload));
   xbee.send(tx);
-
   flashLed(1000);
+
+  // now wait until the global start time (30 seconds) is reached
+  while (millis() + time_offset <= 30000);
+
+  // ITS GO TIME!!!
 }
 
+unsigned int choreo_idx = 0u;
 void loop() {
-  xbee.readPacket();
+  auto const global_now_cs = (millis() + time_offset) / 10;
+  auto const next_onset_cs = pgm_read_word_near(choreo + (choreo_idx * 3));
 
-  if (xbee.getResponse().isAvailable()) {
-    if (xbee.getResponse().getApiId() == RX_16_RESPONSE) {
-      xbee.getResponse().getRx16Response(rx16);
-
-      uint8_t bit_pattern = rx16.getData(suit_number - 1);
-      size_t bit_idx = 0;
-      for (auto const pin : channel_to_pin) {
-        auto const channel_on = static_cast<bool>((bit_pattern >> bit_idx) & 0x01);
-        digitalWrite(pin, channel_on ? HIGH : LOW);
-        ++bit_idx;
-      }      
-    } else {
-      flashLed(50);
-      nss.println("not 16 rx");
+  if (global_now_cs >= next_onset_cs)
+  {
+    // display the current state
+    auto const current_state = pgm_read_byte_near(choreo + (choreo_idx * 3 + 2));
+    size_t bit_idx = 0;
+    for (auto const pin : channel_to_pin) {
+      auto const channel_on = static_cast<bool>((current_state >> bit_idx) & 0x01);
+      digitalWrite(pin, channel_on ? HIGH : LOW);
+      ++bit_idx;
     }
-  } else if (xbee.getResponse().isError()) {
-    nss.println("error");
+
+    // move on to next event
+    ++choreo_idx;
   }
 }
 
-uint8_t get_suit_number() {
-  Serial.write(ni_req_buff, ni_req_buff_len);
-  while (Serial.available() < ni_res_buff_len);
-  uint8_t ni_res_buff[ni_res_buff_len];
-  Serial.readBytes(ni_res_buff, ni_res_buff_len);
-  // 3is the ascii character 0. This hack only works for numbers less than 10
-  const int suit_number = static_cast<int>(ni_res_buff[ni_res_buff_len - 2]) - 48;
-  return suit_number;
-}
 
 void flashLed(unsigned int on_time) {
   // just for debugging
